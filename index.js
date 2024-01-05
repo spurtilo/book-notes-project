7e344af16462a1587c7279de306d27eb6fa70292
Simo Purtilo <148346961+spurtilo@users.noreply.github.com>
2024-01-05 23:09:10 +0200

    Project setup: further setup of node.js backend with required modules, initializing PostgreSQL database, creating the initial frontend components

--- index.js
+++ index.js
@@ -2,14 +2,27 @@ import express from "express";
 import bodyParser from "body-parser";
 import axios from 'axios';
 import pg from "pg";
+import 'dotenv/config';
 
 const app = express();
 const port = 3000;
 
+const db = new pg.Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+// db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+    // const result = await db.query('SELECT * FROM books')
+    // console.log(result.rows);
     res.render('index.ejs')
 });
 
